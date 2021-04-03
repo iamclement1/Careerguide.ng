ea468e5d0c4bcd86534494d4bc3749d106e733d0
Ruthbaby <iroanusi.ruth@gmail.com>
2021-04-03 08:52:18 +0100

    add footer

--- get-started/src/components/App.js
+++ get-started/src/components/App.js
@@ -1,19 +1,29 @@
 //import logo from "./logo.svg";
-//import "./App.css";
-
+import "../App.css";
+import Footer from "./footer";
 import { Container } from "react-bootstrap";
 import SignUp from "./signUp";
 
 function App() {
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <SignUp />
-      </div>
-    </Container>
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{marginBottom: "20px" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <SignUp />
+        </div>
+
+      </Container>
+      <Container
+      style={{ marginTop: "70px" }}
+      >      <div>
+          <Footer />
+        </div>
+
+      </Container>
+    </>
   );
 }
 
---
+++ get-started/src/components/footer.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import loginImg from "../logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Button, Card } from "react-bootstrap";
+import axios from "axios";
+
+export default function Footer() {
+
+    return (
+        <>
+            <div className="footer">
+                <p>Careerguide.ng @copyright 2021</p>
+            </div>
+        </>
+    );
+}
